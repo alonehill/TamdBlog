b7efb5b8f90c7c72ad9fce336b5fb09f8cae7247
孤山寺北贾亭西 <me@gsav.cn>
2026-07-13 22:10:29 +0800

    Clean up header comments in page-links.php
    Removed the header comments and license information from page-links.php.

--- page-links.php
+++ page-links.php
@@ -1,20 +1,5 @@
 <?php
 /**
- * TamdBlog is a theme built on Typecho. It's vibrant and stunning, clear and elegant, with a minimalist white color scheme that makes your site the very definition of elegance
- * 
- * @package Tamd Blog
- * @author KAg Design <3150675236@qq.com,me@gsav.cn>
- * @version 1.0.4
- * @link http://gsav.cn/
- *
- * This file is part of Tamdblog.
- * Tamdblog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version. 
- */
-
-/**
  * 友情链接 (Links)
  *
  * @package custom
